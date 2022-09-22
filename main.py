0f69b1df82d5c895c0c2af4b5145d1f99f22745a
Marcos Antonio <marcosadsjunior@gmail.com>
2022-09-22 01:17:31 -0300

    First step of creating the app

--- main.py
+++ main.py
@@ -2,17 +2,20 @@
 # food recipe and healthy schedule rewarding with points and other things if the user make what it's programed
 
 import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 
-class MyApp(App):
+class FitGameApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return Builder.load_file('fitgame.kv')
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    FitGameApp().run()
